8a5a3ca192c8a8f6f6af03d42dd708c59e2f4375
Eloise <eloise.cresswell@bravissimo.com>
2026-01-27 10:22:05 +0000

    addition of mongo connectoin and vitest - vitest still not fully working - check config jsdom vs node

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
+import { dbConnectionStatus } from "../db/connection-status";
+
+export default async function Home() {
+  const result = await dbConnectionStatus();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hello Eloise</h1>
+        <h1>{"Eloise's Garden"}</h1>
+        <h2>Lets get growing</h2>
+        {result}
       </main>
     </div>
   );
